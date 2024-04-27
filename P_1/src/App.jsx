a4b0f4cd22e0186d75a4626d3b18791856fb53f2
Atul Demond <85558274+atuldemond@users.noreply.github.com>
2024-04-27 13:31:39 +0530

    Update App.jsx

--- P_1/src/App.jsx
+++ P_1/src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './components/Home'
 import Nav from './utils/Nav'
 
 function App() {
