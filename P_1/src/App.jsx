dd75fab8bf5deb45f317bc3496298146c493c75c
atuldemond <atuldemond@gmail.com>
2024-04-28 11:48:52 +0530

    updaing context

--- P_1/src/App.jsx
+++ P_1/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Nav from './utils/Nav'
+import { useState } from "react";
+import Nav from "./utils/Nav";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-        <Nav />
-   
+      <Nav />
     </>
-  )
+  );
+  //!this is checking something
 }
 
-export default App
+export default App;
