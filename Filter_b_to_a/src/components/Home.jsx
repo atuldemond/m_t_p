10e0e08b2bea78750f57792234f8b6367757cda5
atuldemond <atuldemond@gmail.com>
2024-05-07 20:44:27 +0530

    updating just unupdated things still working on filter  .. with axios api

--- Filter_b_to_a/src/components/Home.jsx
+++ Filter_b_to_a/src/components/Home.jsx
@@ -2,38 +2,21 @@ import React, { useContext, useEffect, useState } from "react";
 import Nav from "../utils/Nav";
 import { productdataContext } from "../utils/Context";
 import Loading from "../utils/Loading";
-import axios from "axios";
 
 const Home = () => {
-  // const [products, setproducts] = useContext(productdataContext);
-  const [homedata, sethome] = useState([]);
-
-  const getingapidata = async () => {
-    try {
-      // const { data } =
-      axios.get("https://fakestoreapi.com/products").then((res) => {
-        sethome(res.data);
-        const catego = ["All", ...new Set(homedata.map((val) => val.category))];
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [products, setproducts] = useContext(productdataContext);
 
   useEffect(() => {
-    getingapidata();
+    // const single_category = [...new Set(products.map((val) => val.category))];
+    // console.log(single_category);
   }, []);
 
-  return homedata ? (
+  return products ? (
     <div className="main flex  w-screen h-[400vh] ">
-      <div className="category  bg-red-200 w-[15vw] ">
-        {/* {catego.map((val) => (
-          <>hii</>
-        ))} */}
-      </div>
+      <div className="category  flex flex-col  gap-2 bg-red-200 w-[15vw] "></div>
 
       <div className="w-[100vw] h-[400vh] bg-blue-300 justify-center  flex flex-wrap ">
-        {homedata.map((a, i) => (
+        {products.map((a, i) => (
           <div
             key={a.id}
             className="cards w-[13vw] h-[60vh] my-[3vh] mx-[3vw] flex flex-col items-center  px-[1vw] py-[1vh] bg-white"
--- Filter_b_to_a/src/utils/Loading.jsx
+++ Filter_b_to_a/src/utils/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center text-6xl font-bolder w-[full] h-[full]">
+    <div className="flex justify-center my-[35vh] items-center text-6xl font-bolder w-[full] h-[full]">
       LOADING....
     </div>
   );
